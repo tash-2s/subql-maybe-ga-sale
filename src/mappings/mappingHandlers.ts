5fd295cfbf8e2f5ce27cebe87eaf185a84325673
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-09-07 23:14:59 +0900

    WIP

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,19 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
-
-
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
-}
-
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
+import { SubstrateExtrinsic } from "@subql/types";
+import { Sale } from "../types";
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
+  const record = new Sale(extrinsic.block.block.header.hash.toString()); // TODO: not correct
+  const call = extrinsic.extrinsic.method.args[0][0] as any;
+  if (
+    call.section === "system" &&
+    call.method === "remark" &&
+    call.args
+      .toString()
+      .startsWith(
+        "RMRK::BUY::1.0.0::7479313-7a147cba01afccbf17-GA-GENERATIVE_ASTERISM_"
+      )
+  ) {
+    record.nftN = parseInt(call.args.toString().slice(-3), 10);
     await record.save();
+  }
 }
-
-
