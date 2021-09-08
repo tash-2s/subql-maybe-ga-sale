1f13de688e5b55bbf1f822bd8d27d981e8eb2e74
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-09-08 14:57:39 +0900

    Works?

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,18 +2,29 @@ import { SubstrateExtrinsic } from "@subql/types";
 import { Sale } from "../types";
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-  const record = new Sale(extrinsic.block.block.header.hash.toString()); // TODO: not correct
   const call = extrinsic.extrinsic.method.args[0][0] as any;
   if (
     call.section === "system" &&
     call.method === "remark" &&
-    call.args
-      .toString()
-      .startsWith(
+    call.args.toString().startsWith("0x524d524b")
+  ) {
+    const str = Buffer.from(
+      call.args.toString().replace(/^0x/, ""),
+      "hex"
+    ).toString();
+    if (
+      str.startsWith(
         "RMRK::BUY::1.0.0::7479313-7a147cba01afccbf17-GA-GENERATIVE_ASTERISM_"
       )
-  ) {
-    record.nftN = parseInt(call.args.toString().slice(-3), 10);
-    await record.save();
+    ) {
+      const id = `${extrinsic.block.block.header.hash.toString()}-${
+        extrinsic.idx
+      }`;
+      const nftN = parseInt(str.slice(-3), 10);
+      logger.info(`found: ${id}, ${nftN}`);
+      const record = new Sale(id);
+      record.nftN = nftN;
+      await record.save();
+    }
   }
 }
